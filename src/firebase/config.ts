dfb9ab8d611f391c3fd303c0dd977a6df496c201
SUDHIR NANDHAN M <contactsudhirnandhan@gmail.com>
2026-03-16 11:12:16 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8288116234-400b6",
+  "appId": "1:476185799602:web:2a29e93e0d2f23e40492ab",
+  "apiKey": "AIzaSyCkaWnqIIYaskLv6MgiNHUr4Z8bShYBrIA",
+  "authDomain": "studio-8288116234-400b6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "476185799602"
+};
