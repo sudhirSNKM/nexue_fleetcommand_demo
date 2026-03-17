90beb1bd205bae6ddf3fa9174c88890f73b7e264
SUDHIR NANDHAN M <contactsudhirnandhan@gmail.com>
2026-03-17 04:39:01 +0000

    Build Error
    Export firebaseConfig doesn't exist in target module
    
    ./sr

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-8288116234-400b6",
-  "appId": "1:476185799602:web:2a29e93e0d2f23e40492ab",
-  "apiKey": "AIzaSyCkaWnqIIYaskLv6MgiNHUr4Z8bShYBrIA",
-  "authDomain": "studio-8288116234-400b6.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "476185799602"
+  apiKey: "AIzaSyCkaWnqIIYaskLv6MgiNHUr4Z8bShYBrIA",
+  authDomain: "studio-8288116234-400b6.firebaseapp.com",
+  projectId: "studio-8288116234-400b6",
+  storageBucket: "studio-8288116234-400b6.firebasestorage.app",
+  messagingSenderId: "476185799602",
+  appId: "1:476185799602:web:2a29e93e0d2f23e40492ab"
 };
